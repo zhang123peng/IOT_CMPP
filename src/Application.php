99b435ed120d2edb631269b248ac5904c71df96f
Bill Zhang <708947679@qq.com>
2019-04-26 11:08:47 +0800

    第一次开发上传

---
+++ src/Application.php
@@ -0,0 +1,13 @@
+<?php
+namespace billzhang\iot_cmpp;
+
+/**
+ * Class Application
+ * @package billzhang\iot_cmpp
+ */
+class Application
+{
+    public static function index(){
+        return 1;
+    }
+}
